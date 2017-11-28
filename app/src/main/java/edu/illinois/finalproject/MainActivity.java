4b27226b1f6afa33ed8a01e2cfb9519a9a45b89c
icebpal <bpl2@illinois.edu>
2017-11-27 18:59:05 -0600

    Intial commit with all the photo taking and uploading code. Also Firebase setup and being able to share photos.

--- app/src/main/java/edu/illinois/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/finalproject/MainActivity.java
@@ -1,13 +1,90 @@
 package edu.illinois.finalproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int REQUEST_IMAGE_CAPTURE = 1;
+    final int PICK_IMAGE = 2;
+    private ImageView imageView;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        this.imageView = (ImageView) findViewById(R.id.thumbImage);
+        Button camera = (Button) findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+        Button upload = (Button) findViewById(R.id.uploadPhoto);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+    }
+
+    /**
+     * Method to run after the main activity ends, which in this case will be after the user has
+     * taken or uploaded a picture. Then will get that picture and display it on the screen.
+     *
+     * @param requestCode code for whether it was a picture being taken or an upload
+     * @param resultCode code checking that the picture was taken/uploaded successfully
+     * @param data the intent of the picture taken/uploaded
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imageView.setImageBitmap(selectedImage);
+
+                StorageReference imageRef = mStorageRef.child(imageUri.toString());
+                imageRef.putFile(imageUri);
+
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
